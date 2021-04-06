fbc352f6456b489c8f6f511cfa24bcae1c35efd6
Carli Nabbe <carli_nabbe@hotmail.com>
2021-04-06 14:46:22 +0200

    Added scss, area svg in map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { map } from '../img/Strandschermkaart-full.jpg'
+import map from '../img/Strandschermkaart-full.jpg'
+// import area from '../img/area.svg'
+
+import '../styles/map.scss'
 
 export default function Map() {
     return (
-        <div>
+        <div className="map">
+            {/* <img src= { area } alt="Area of one part" className="map__area-img"></img> */}
+            <svg className="map__area-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 387 374"><g data-name="Layer 2"><path fill="#5bcc1b" opacity=".4" d="M372 21l-114 2-117-8L0 0l87 374 300-23-15-330z" data-name="Layer 1"/></g></svg>
             <img src= { map } alt="Map of area"></img>
         </div>
     )
